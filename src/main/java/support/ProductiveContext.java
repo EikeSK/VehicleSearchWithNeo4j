ac275bb8530bfa1204b6e45dc81a893f2c017b99
Eike <eike@schulte-kersmecke.de>
2014-12-09 11:15:30 +0100

    added productive context configuration class for spring

---
+++ src/main/java/support/ProductiveContext.java
@@ -0,0 +1,45 @@
+package support;
+
+import org.neo4j.graphdb.GraphDatabaseService;
+import org.neo4j.test.TestGraphDatabaseFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.neo4j.config.EnableNeo4jRepositories;
+import org.springframework.data.neo4j.config.Neo4jConfiguration;
+import org.springframework.data.neo4j.rest.SpringRestGraphDatabase;
+import org.springframework.data.neo4j.support.Neo4jTemplate;
+import repositories.TermRepository;
+import repositories.VehicleModelRepository;
+import service.VehicleDataPersistenceService;
+
+@Configuration
+@EnableNeo4jRepositories(basePackages = "repositories")
+public class ProductiveContext extends Neo4jConfiguration {
+
+    public ProductiveContext() {
+        setBasePackage("domain", "repositories");
+    }
+
+    @Bean
+    public GraphDatabaseService graphDatabaseService() {
+        return new SpringRestGraphDatabase("http://localhost:7474/db/data");
+    }
+
+    @Bean
+    public Neo4jTemplate neo4jTemplate() {
+        return new Neo4jTemplate(graphDatabaseService());
+    }
+
+    @Bean
+    public VehicleDataPersistenceService vehicleDataPersistenceService() {
+        return new VehicleDataPersistenceService(vehicleModelRepository, termRepository);
+    }
+
+    @Autowired
+    public VehicleModelRepository vehicleModelRepository;
+
+    @Autowired
+    public TermRepository termRepository;
+
+}
