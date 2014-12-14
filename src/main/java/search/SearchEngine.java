b765bebfa7bb8f62e4d12d93224288bcbaca01e3
Eike <eike@schulte-kersmecke.de>
2014-12-14 12:08:36 +0100

    started implementation of SearchEngine

--- src/main/java/search/SearchEngine.java
+++ src/main/java/search/SearchEngine.java
@@ -1,10 +1,26 @@
 package search;
 
 import domain.VehicleNode;
+import support.StringSplitterUtils;
+import support.VehicleNodeSearchQuery;
+import support.VehicleSearchQueryGenerator;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Set;
 
 public class SearchEngine {
 
-    public VehicleNode search(final String searchString) {
-        return null;
+    private final VehicleSearchQueryGenerator _vehicleSearchQueryGenerator;
+
+    public SearchEngine(VehicleSearchQueryGenerator vehicleSearchQueryGenerator) {
+        _vehicleSearchQueryGenerator = vehicleSearchQueryGenerator;
+    }
+
+    public Collection<VehicleNode> search(final String searchString) {
+        final Collection<VehicleNode> searchResults = new ArrayList<>();
+        final Set<String> tokensFromSearchString = StringSplitterUtils.tokenize(searchString);
+        final VehicleNodeSearchQuery vehicleNodeSearchQuery = _vehicleSearchQueryGenerator.generateSearchQueryFrom(tokensFromSearchString);
+        return searchResults;
     }
 }
