d01c7c505a03a2b5e5f88342aae33b407e3d08fe
Eike <eike@schulte-kersmecke.de>
2014-12-16 13:32:43 +0100

    added more test data

--- src/test/java/service/VehicleDataHelperTest.java
+++ src/test/java/service/VehicleDataHelperTest.java
@@ -20,12 +20,16 @@ public class VehicleDataHelperTest {
 
     @Test
     public void fillDatabaseWithVehicleData() throws Exception {
-        _vehicleDataPersistenceService.tokenizeAndSave(vehicleModelWithName("Audi A4 B8 Kombi"));
-        _vehicleDataPersistenceService.tokenizeAndSave(vehicleModelWithName("Audi A6 B6 Kombi"));
+        _vehicleDataPersistenceService.tokenizeAndSave(vehicleModelWithName("Audi A4 B8 Kombi"), new HashSet<>(Arrays.asList("Avant")));
+        _vehicleDataPersistenceService.tokenizeAndSave(vehicleModelWithName("Audi A6 B6 Kombi"), new HashSet<>(Arrays.asList("Avant")));
         _vehicleDataPersistenceService.tokenizeAndSave(vehicleModelWithName("BMW 1er E87 Coupe"));
-        _vehicleDataPersistenceService.tokenizeAndSave(vehicleModelWithName("BMW 1er F21 Coupe"));
+        _vehicleDataPersistenceService.tokenizeAndSave(vehicleModelWithName("BMW 1er F21 Coupe"), new HashSet<>(Arrays.asList("neuster","neuer","2012")));
         _vehicleDataPersistenceService.tokenizeAndSave(vehicleModelWithName("VW Bora 1J Limousine"));
-        _vehicleDataPersistenceService.tokenizeAndSave(vehicleModelWithName("VW Golf 7 Kombi"), new HashSet<>(Arrays.asList("benzin", "2008")));
+        _vehicleDataPersistenceService.tokenizeAndSave(vehicleModelWithName("Audi A3 8P Cabrio"), new HashSet<>(Arrays.asList("2003", "2013")));
+        _vehicleDataPersistenceService.tokenizeAndSave(vehicleModelWithName("VW Golf 7 Kombi"), new HashSet<>(Arrays.asList("seit", "2012", "neuster", "neuer")));
+        _vehicleDataPersistenceService.tokenizeAndSave(vehicleModelWithName("VW Golf 6 Kombi"), new HashSet<>(Arrays.asList("2008", "2012", "diesel", "benzin")));
+        _vehicleDataPersistenceService.tokenizeAndSave(vehicleModelWithName("Skoda Octavia Scout Kombi"), new HashSet<>(Arrays.asList("2007", "2012", "diesel", "benzin")));
+        _vehicleDataPersistenceService.tokenizeAndSave(vehicleModelWithName("Skoda Octavia E5 Limousine"), new HashSet<>(Arrays.asList("seit", "2013", "neuster", "neuer")));
     }
 
     private VehicleNode vehicleModelWithName(final String name) {
