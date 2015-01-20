099975a78f99f9f48b213dabe3112cba3e9c4fdd
Eike <eike@schulte-kersmecke.de>
2015-01-20 10:21:44 +0100

    updated to new version of spring data (3.3.0.M1) to gain better performance

--- src/main/java/config/ProductiveContext.java
+++ src/main/java/config/ProductiveContext.java
@@ -6,7 +6,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.neo4j.config.EnableNeo4jRepositories;
 import org.springframework.data.neo4j.config.Neo4jConfiguration;
-import org.springframework.data.neo4j.rest.SpringRestGraphDatabase;
+import org.springframework.data.neo4j.rest.SpringCypherRestGraphDatabase;
 import org.springframework.data.neo4j.support.Neo4jTemplate;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 import repositories.BaujahrNodeRepository;
@@ -36,7 +36,7 @@ public class ProductiveContext extends Neo4jConfiguration {
 
     @Bean
     public GraphDatabaseService graphDatabaseService() {
-        return new SpringRestGraphDatabase("http://localhost:7474/db/data");
+        return new SpringCypherRestGraphDatabase("http://localhost:7474/db/data");
     }
 
     @Bean
