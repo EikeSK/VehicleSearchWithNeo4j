0c7efee3f05045df804ff86b0db0bd7d22f35337
Eike <eike@schulte-kersmecke.de>
2015-02-09 11:20:59 +0100

    some small changes

--- src/test/java/service/VehicleDataHelperTest.java
+++ src/test/java/service/VehicleDataHelperTest.java
@@ -14,6 +14,9 @@ import java.util.*;
 import static support.TestUtils.vehicleMetaDataWithTerms;
 import static support.TestUtils.vehicleMetaDataWithTermsAndBaujahr;
 
+/**
+ * Test zum Befüllen der produktiven Datenbank mit einem Test-Datensatz zu Demonstrationszwecken.
+ */
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(classes = {ProductiveContext.class})
 public class VehicleDataHelperTest {
@@ -26,10 +29,10 @@ public class VehicleDataHelperTest {
         Map<VehicleNode, VehicleMetaData> batch = new HashMap<>();
         batch.put(vehicleNodeWithName("Audi A4 B8 Kombi"), vehicleMetaDataWithTerms(new HashSet<>(Arrays.asList("Avant"))));
         batch.put(vehicleNodeWithName("Audi A6 B6 Kombi"), vehicleMetaDataWithTerms(new HashSet<>(Arrays.asList("Avant"))));
+        batch.put(vehicleNodeWithName("Audi A3 8P Cabrio"), vehicleMetaDataWithTermsAndBaujahr(new HashSet<>(Arrays.asList("2003", "2013")), 2003, 2013));
         batch.put(vehicleNodeWithName("BMW 1er E87 Coupe"), vehicleMetaDataWithTerms(Collections.<String>emptySet()));
         batch.put(vehicleNodeWithName("BMW 1er F21 Coupe"), vehicleMetaDataWithTermsAndBaujahr(new HashSet<>(Arrays.asList("neuster", "neuer", "2012")), 2012, 2014));
         batch.put(vehicleNodeWithName("VW Bora 1J Limousine"), vehicleMetaDataWithTerms(Collections.<String>emptySet()));
-        batch.put(vehicleNodeWithName("Audi A3 8P Cabrio"), vehicleMetaDataWithTermsAndBaujahr(new HashSet<>(Arrays.asList("2003", "2013")), 2003, 2013));
         batch.put(vehicleNodeWithName("VW Golf 7 Kombi"), vehicleMetaDataWithTermsAndBaujahr(new HashSet<>(Arrays.asList("seit", "2012", "neuster", "neuer")), 2012, 2014));
         batch.put(vehicleNodeWithName("VW Golf 6 Kombi"), vehicleMetaDataWithTermsAndBaujahr(new HashSet<>(Arrays.asList("2008", "2012", "diesel", "benzin")), 2008, 2012));
         batch.put(vehicleNodeWithName("Skoda Octavia Scout Kombi"), vehicleMetaDataWithTermsAndBaujahr(new HashSet<>(Arrays.asList("2007", "2012", "diesel", "benzin")), 2007, 2012));
